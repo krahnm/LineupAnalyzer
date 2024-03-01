8158f7878c829136590c7eb9417f9e037a6b5fc7
Matthew Krahn <krahnm2000@gmail.com>
2024-03-01 15:27:20 -0600

    Initial code to insert festival information into festival_table

--- Program.cs
+++ Program.cs
@@ -10,9 +10,15 @@ using Swan;
 using Swan.Logging;
 
 using SpotifyAPI;
-using LineupAnalyzer.Spotify;
+using LineupAnalyzer.Models;
 using SpotifyAPI.Web;
 using LineupAnalyzer.Helpers;
+using Postgrest.Attributes;
+using Postgrest.Models;
+using System.Reflection;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using LineupAnalyzer.Services;
+using LineupAnalyzer.DataAccess;
 
 /*
  TODO: Add Try/Catch around Spotify requests to handle errors
@@ -21,12 +27,68 @@ using LineupAnalyzer.Helpers;
 
 class Program
 {
-
+   
     static async Task Main()
     {
 
-        SpotifyInfo spotifyInfo = new SpotifyInfo();
+        //Console.WriteLine("Welcome to the Lineup Analyzer");
+        //SpotifyInfo spotifyInfo = new SpotifyInfo();
+        //Console.WriteLine("Created SpotifyInfo");
+
+        // FestivalInfo festival = new FestivalInfo();
+        //Console.WriteLine("Created FestivalInfo");
+        //festival.TestFestivalInsert();
+        //Console.WriteLine("Inserted Test Festival");
+
+        FestivalTable data = new FestivalTable
+        {
+            FestivalName = "Lollapalooza Chicago",
+            Year = DateTime.Parse("08-03-2023"),
+            Dates = "{\"Dates\": [08-03-2023,08-04-2023,08-05-2023]}",
+            City = "Chicago",
+            Province = "Illinois",
+            Country = "USA",
+            PopularityStats = "{}",
+            GenreStats = "{}",
+
+        };
+
+        // Retrieve Supabase Secrets
+        IConfigurationRoot appConfig = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+        // Used to connect to Supabase project
+        var url = appConfig["SupabaseURL"];
+        var key = appConfig["SupabaseKey"];
+
+        var options = new Supabase.SupabaseOptions
+        {
+            AutoConnectRealtime = true,
+            AutoRefreshToken = true
+        };
+
+        // Used to Authenticate User
+        var email = appConfig["SupabaseEmail"];
+        var password = appConfig["SupabasePass"];
+
+        // Initialize the Supabase client
+        var client = new Supabase.Client(url, key, options);
+        await client.InitializeAsync();
         
+
+        Console.WriteLine("Supabase Client Initialized");
+
+        if (client == null)
+        {
+            Console.WriteLine("supabase is null here");
+        }
+        // Obtain a user session
+        await client.Auth.SignIn(email, password);
+
+        await client.From<FestivalTable>().Insert(data);
+
+
         /*
         FullArtist artist = await spotifyInfo.GetFullArtist("Foals");
 
@@ -41,15 +103,16 @@ class Program
         }*/
 
         //TODO: Make a lineup class?
-        string filePath = "C:/Users/Krahnm/Documents/LineupAnalyzer/Resources/Osheaga2023Lineup.csv";
-        List<string> artistList = FileIO.ReadArtistListCSV(filePath);
-        Console.WriteLine("Artist list is " + artistList.Count + " acts long.");
-        List<FullArtist> lineup = await spotifyInfo.GetFullArtistList(artistList);
+        /* string filePath = "C:/Users/Krahnm/Documents/LineupAnalyzer/Resources/Osheaga2023Lineup.csv";
+         List<string> artistList = FileIO.ReadArtistListCSV(filePath);
+         Console.WriteLine("Artist list is " + artistList.Count + " acts long.");
+         List<FullArtist> lineup = await spotifyInfo.GetFullArtistList(artistList);
 
-        for (int i = 0; i < lineup.Count; i++)
-        {
-            Console.WriteLine(lineup[i].Popularity);
-    
-        }
+         for (int i = 0; i < lineup.Count; i++)
+         {
+             Console.WriteLine(lineup[i].Popularity);
+
+         }*/
+
     }
 }
